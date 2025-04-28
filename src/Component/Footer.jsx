91497248ed8fab91dd0235bc0e1d057c823376e0
Malik Sheryar Shoukat <128922124+MalikSheryar@users.noreply.github.com>
2025-04-28 17:10:13 +0500

    Push new code

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -21,21 +21,6 @@ export default function Footer() {
             Follow us:
             </p>
         </div>
-        <div className="para">
-            <p className="font-[Inter] text-white text-center">
-            Follow us:
-            </p>
-        </div>
-        <div className="para">
-            <p className="font-[Inter] text-white text-center">
-            Follow us:
-            </p>
-        </div>
-        <div className="para">
-            <p className="font-[Inter] text-white text-center">
-            Follow us:
-            </p>
-        </div>
         <div className="img cursor-pointer">
             <img src={img40} alt="" />
             </div>
